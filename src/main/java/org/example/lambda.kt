c7971cfa99a471bbfa35e05f1e100f17fd0e5985
Fatom Al-Ghanmi <fatom.alghanmi@triosstudent.com>
2025-11-12 10:56:28 -0500

    added lambda

---
+++ src/main/java/org/example/lambda.kt
@@ -0,0 +1,18 @@
+package org.example
+
+fun main() {
+
+    var multiplyLambda = { a: Int, b: Int -> Int
+        a * b
+    }
+
+    multiplyLambda = { a, b -> // Shorthand version
+        a * b
+    }
+
+    val lambdaResult = multiplyLambda(4, 2)
+
+    println(lambdaResult)
+
+}
+
